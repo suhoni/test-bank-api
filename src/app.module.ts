a54106d80a5dcf95fb2b03f20cd9a1ebe55a31ea
Anastasia <dekamoru@gmail.com>
2026-03-08 05:43:41 +0300

    made db migrations and created modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { OperationsModule } from './operations/operations.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AccountsModule, OperationsModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/migrations/1772928509734-init.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Init1772928509734 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        CREATE TABLE users
+        (
+            id         uuid PRIMARY KEY,
+            email      varchar NOT NULL UNIQUE,
+            created_at timestamp DEFAULT now()
+        )
+    `);
+
+    await queryRunner.query(`
+        CREATE TABLE accounts
+        (
+            id         uuid PRIMARY KEY,
+            user_id    uuid NOT NULL,
+            balance    numeric(15, 2) DEFAULT 0,
+            created_at timestamp      DEFAULT now(),
+
+            CONSTRAINT fk_user
+                FOREIGN KEY (user_id)
+                    REFERENCES users (id)
+                    ON DELETE CASCADE,
+
+            CONSTRAINT balance_positive
+                CHECK (balance >= 0)
+        )
+    `);
+
+    await queryRunner.query(`
+        CREATE TABLE transactions
+        (
+            id              uuid PRIMARY KEY,
+            type            varchar        NOT NULL,
+            amount          numeric(15, 2) NOT NULL,
+
+            from_account_id uuid,
+            to_account_id   uuid           NOT NULL,
+
+            created_at      timestamp DEFAULT now(),
+
+            FOREIGN KEY (from_account_id) REFERENCES accounts (id),
+            FOREIGN KEY (to_account_id) REFERENCES accounts (id)
+        )
+    `);
+
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE transactions`);
+    await queryRunner.query(`DROP TABLE accounts`);
+    await queryRunner.query(`DROP TABLE users`);
+  }
+
+}
